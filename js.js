d1da8d006bcb15d58e77df4625089ea0559a0a96
olya-qznecova <35519547+olya-qznecova@users.noreply.github.com>
2018-02-22 10:38:27 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
     $('.menu a').click(function(event) {
         event.preventDefault(); // отменяем стандартное действие браузера при событии "клик" (клик по ссылке без перехода на новый URL)
         $('.menu .active').removeClass('active'); // убираем у активного элемена класс active
